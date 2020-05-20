26b2a5951d7999e7e3a62931d909c6f328f6abc1
shibadog <shibadog@users.noreply.github.com>
2020-05-20 23:22:33 +0900

    ダブルクウォート付きの引数を許容

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,10 @@ export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
-echo "jmeter args=$@"
+echo "jmeter args=$*"
 
 # Keep entrypoint simple: we must pass the standard JMeter arguments
-jmeter $@
+jmeter $*
 echo "END Running Jmeter on `date`"
 
 #     -n \
